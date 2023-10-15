dcd10ce226b545198ea2ba1e82ef5c86b1e6065c
Alexandria Stephens <alexandria26999@gmail.com>
2023-10-15 15:03:42 -0600

    needs reworked

--- script.js
+++ script.js
@@ -5,24 +5,52 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 function search(str) {
 	let results = [];
-
-	// TODO
-
+	let filtered = fruit.map(item => item.toLowerCase()).filter(item => item.includes(str));
+	if (filtered.length > 0) {
+		results = [...filtered];
+	}
 	return results;
 }
 
-function searchHandler(e) {
-	// TODO
-}
-
 function showSuggestions(results, inputVal) {
-
-	// TODO
+inputVal = input.value;
+suggestions.classList.add("has-suggestions");
+if (suggestions.children.length !== results.length) {
+	let toRemove = Array.from(suggestions.children).filter(item => !item.id.includes(inputVal));
+	toRemove.forEach(item => 
+		suggestions.removeChild(item.parentElement));
 }
+results.forEach(item => {
+	if (document.getElementById(item) === null){
+	let div = document.createElement("div");
+	let listItem = document.createElement("li");
+	suggestions.appendChild(div);
+	div.appendChild(listItem);
+	listItem.id = item;
+	listItem.innerText = item;
+	listItem.classList.add("suggestion");
+	listItem.addEventListener("click", useSuggestion);
+	}
+})
+}
+
+
+function searchHandler(e) {
+let str = e.target.value.trim().toLowerCase();
+if (str.length > 0) {
+	let results = search(str);
+	showSuggestions(results);
+}
+}
+
+
 
 function useSuggestion(e) {
-	// TODO
+input.value = e.target.innerText;
+suggestions.classList.remove("has-suggestions");
+while (suggestions.children.length > 0) {
+	suggestions.removeChild(suggestions.lastElementChild);
+}
 }
 
 input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
